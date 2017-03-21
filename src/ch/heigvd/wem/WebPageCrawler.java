497169ec5237442738f3912b66c6f89ea65536c9
Axel Fahy <axel@fahy.net>
2017-03-21 16:47:11 +0100

    fit(crawler): add basic crawler based on documentation
    Link to doc: https://github.com/yasserg/crawler4j

--- src/ch/heigvd/wem/WebPageCrawler.java
+++ src/ch/heigvd/wem/WebPageCrawler.java
@@ -9,49 +9,58 @@ import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.parser.ParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
 
+import java.util.regex.Pattern;
+
 public class WebPageCrawler extends WebCrawler {
 
-	private static WebPageIndexerQueue indexer = null;
+    private static WebPageIndexerQueue indexer = null;
+    private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg"
+            + "|png|mp3|zip|gz))$");
 
-	@Override
-	public boolean shouldVisit(Page referringPage, WebURL url) {
-		/* A IMPLEMENTER */
-		
-		if(Labo1.DEBUG) {
-			System.out.println("shouldVisit called");
-			System.out.println("Referring page: " + referringPage.getWebURL());
-			System.out.println("Url: " + url);
-		}
-		
-		return true;
-	}
-	
-	@Override
-	public void visit(Page page) {
-		
-		if(indexer == null) {
-			System.err.println("WebPageSaver doesn't contains a WebPageIndexer, you must set it in Labo1.java before starting the crawling");
-			System.exit(1);
-		}
+    @Override
+    public boolean shouldVisit(Page referringPage, WebURL url) {
+        if (Labo1.DEBUG) {
+            System.out.println("shouldVisit called");
+            System.out.println("Referring page: " + referringPage.getWebURL());
+            System.out.println("Url: " + url);
+        }
 
-		Metadata metadata = new Metadata();
-		String content = null;
-		
-		ParseData data = page.getParseData();
-		if(data instanceof HtmlParseData) {
-			HtmlParseData htmlData = (HtmlParseData) data;
-			metadata.setTitle(htmlData.getTitle());
-			metadata.setUrl(page.getWebURL());
-			metadata.setLinks(htmlData.getOutgoingUrls());
-			content = htmlData.getText();
-		}
+        String href = url.getURL().toLowerCase();
+        return !FILTERS.matcher(href).matches()
+                && href.startsWith("http://www.ics.uci.edu/");
+    }
 
-		//we queue the page for indexer
-		VisitedPage visitedPage = new VisitedPage(metadata, content);
-		indexer.queueVisitedPage(visitedPage);
-	}
+    @Override
+    public void visit(Page page) {
 
-	public static void setIndexerQueue(WebPageIndexerQueue indexer) { WebPageCrawler.indexer = indexer; }
-	public static WebPageIndexerQueue getIndexerQueue() { return indexer; }
-	
+        if (indexer == null) {
+            System.err.println("WebPageSaver doesn't contains a WebPageIndexer, you must set it in Labo1.java before starting the crawling");
+            System.exit(1);
+        }
+
+        Metadata metadata = new Metadata();
+        String content = null;
+
+        ParseData data = page.getParseData();
+        if (data instanceof HtmlParseData) {
+            HtmlParseData htmlData = (HtmlParseData) data;
+            metadata.setTitle(htmlData.getTitle());
+            metadata.setUrl(page.getWebURL());
+            metadata.setLinks(htmlData.getOutgoingUrls());
+            content = htmlData.getText();
+        }
+
+        // We queue the page for indexer
+        VisitedPage visitedPage = new VisitedPage(metadata, content);
+        indexer.queueVisitedPage(visitedPage);
+    }
+
+    public static void setIndexerQueue(WebPageIndexerQueue indexer) {
+        WebPageCrawler.indexer = indexer;
+    }
+
+    public static WebPageIndexerQueue getIndexerQueue() {
+        return indexer;
+    }
+
 }
