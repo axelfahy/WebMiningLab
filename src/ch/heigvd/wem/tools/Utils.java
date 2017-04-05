61bfa7ec993effd88483a13eb362c367c6fb91c5
Axel Fahy <axel@fahy.net>
2017-04-05 14:33:14 +0200

    feat(tokenizer): add a tokenizer function
    Add a tokneizer function in the Utils.java file.
    This method is static and used to tokenize text.
    Apostrophes are not used as separator and are removed if at
    the end of a word.

---
+++ src/ch/heigvd/wem/tools/Utils.java
@@ -0,0 +1,27 @@
+package ch.heigvd.wem.tools;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Utils {
+
+    /**
+     * Tokenization on space and punctuation except apostrophes
+     *
+     * If an apostrophe is at the end of a word, removes it.
+     *
+     * @param s String to tokenize
+     * @return List of tokens (String)
+     */
+    public static List<String> tokenize(String s) {
+        List<String> tokens = Arrays.asList(s.split("[^\\w']+"));
+
+        // Remove apostrophe if at the end of a word
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens.get(i).substring(tokens.get(i).length() - 1).equals("'")) {
+                tokens.set(i, tokens.get(i).substring(0, tokens.get(i).length() - 1));
+            }
+        }
+        return tokens;
+    }
+}
