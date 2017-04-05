b6a454d33984bcc2da9078e1f4164cf6442403c6
Axel Fahy <axel@fahy.net>
2017-04-05 14:46:02 +0200

    feat(retriever): implementation of executeQuery method
    Implementation of executeQuery method.
    Exception are not handle yet.

--- src/ch/heigvd/wem/labo1/WebPageRetriever.java
+++ src/ch/heigvd/wem/labo1/WebPageRetriever.java
@@ -2,9 +2,9 @@ package ch.heigvd.wem.labo1;
 
 import ch.heigvd.wem.interfaces.Index;
 import ch.heigvd.wem.interfaces.Retriever;
+import ch.heigvd.wem.tools.Utils;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class WebPageRetriever extends Retriever {
 
@@ -56,6 +56,75 @@ public class WebPageRetriever extends Retriever {
 
     @Override
     public Map<Long, Double> executeQuery(String query) {
-        return null;
+        // Tokenization on space and punctuation except apostrophes
+        List<String> tokens = Utils.tokenize(query);
+
+        Map<Long, Double> scores = new HashMap<>();
+        Map<Long, Double> lengthsDoc = new HashMap<>();
+        Double lengthsQuery = 0.0;
+
+        // Calculate frequencies of terms from query
+        Map<String, Long> frequencies = new HashMap<>();
+        for (String token : tokens) {
+            if (!frequencies.containsKey(token))
+                frequencies.put(token, 0L);
+
+            frequencies.put(token, frequencies.get(token) + 1);
+            lengthsQuery += Math.pow(frequencies.get(token) ,2);
+        }
+
+        for (String token : tokens) {
+            Long freq = frequencies.get(token);
+            Map<Long, Weights> postings = this.index.getInvertedIndex().get(token);
+
+            // Run over each document
+            for (Map.Entry<Long, Weights> entry : postings.entrySet()) {
+                // Calculate the score
+                if (!scores.containsKey(entry.getKey()))
+                    scores.put(entry.getKey(), 0.0);
+                scores.put(entry.getKey(), scores.get(entry.getKey()) + freq * entry.getValue().getFrequency());
+
+                // Calculate the length
+                if (!lengthsDoc.containsKey(entry.getKey()))
+                    lengthsDoc.put(entry.getKey(), 0.0);
+                lengthsDoc.put(entry.getKey(), lengthsDoc.get(entry.getKey()) + Math.pow(entry.getValue().getFrequency(), 2));
+            }
+        }
+        // Run over each document and calculate the length
+        for (Map.Entry<Long, Map<String, Weights>> entry : this.index.getIndex().entrySet()) {
+            // Calculate the length
+            if (!lengthsDoc.containsKey(entry.getKey()))
+                lengthsDoc.put(entry.getKey(), 0.0);
+
+            for (Map.Entry<String, Weights> entryTerm : entry.getValue().entrySet()) {
+                lengthsDoc.put(entry.getKey(), lengthsDoc.get(entry.getKey()) + Math.pow(entryTerm.getValue().getFrequency(), 2));
+            }
+        }
+
+        // Calculate the final lengths
+        for (Map.Entry<Long, Double> entry : lengthsDoc.entrySet()) {
+            lengthsDoc.put(entry.getKey(), Math.sqrt(lengthsQuery * entry.getValue()));
+        }
+
+        // Calculate the final score (score[d] / length[d])
+        for (Map.Entry<Long, Double> entry : scores.entrySet()) {
+            scores.put(entry.getKey(), entry.getValue() / lengthsDoc.get(entry.getKey()));
+        }
+
+        Set<Map.Entry<Long, Double>> set = scores.entrySet();
+        List<Map.Entry<Long, Double>> list = new ArrayList<>(set);
+        Collections.sort(list, new Comparator<Map.Entry<Long, Double>>() {
+            public int compare(Map.Entry<Long, Double> o1,
+                    Map.Entry<Long, Double> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+
+        Map<Long, Double> results = new HashMap<>();
+        for(Map.Entry<Long, Double> entry : list) {
+            results.put(entry.getKey(), entry.getValue());
+        }
+
+        return results;
     }
 }
