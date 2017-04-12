03dca39fba9cb2d4b0ad9e7a769b9f8561e2ccbc
Axel Fahy <axel@fahy.net>
2017-04-12 09:34:26 +0200

    chore(linkanalysis): reformat code

--- src/ch/heigvd/wem/linkanalysis/GraphFileReader.java
+++ src/ch/heigvd/wem/linkanalysis/GraphFileReader.java
@@ -22,92 +22,96 @@ import java.util.List;
  * node2;node3
  * mode3;node1
  * </code></pre>
+ *
  * @author Florian Poulin <i>(florian.poulin at heig-vd.ch)</i>
  */
 public class GraphFileReader {
 
-	private AdjacencyMatrix am;
-	private LinkedHashMap<String,Integer> map;
+    private AdjacencyMatrix am;
+    private LinkedHashMap<String, Integer> map;
 
-	/**
-	 * Constructor. Parses the file with the given name.
-	 * @param filename Name of the file to be parsed. See format instructions in the 
-	 * class comments.
-	 */
-	public GraphFileReader (String filename) {
+    /**
+     * Constructor. Parses the file with the given name.
+     *
+     * @param filename Name of the file to be parsed. See format instructions in the
+     *                 class comments.
+     */
+    public GraphFileReader(String filename) {
 
-		// Init structures for storing information
-		List<String> nodes = new LinkedList<String>();
-		HashMap<String, LinkedList<String>> edges = 
-				new HashMap<String, LinkedList<String>>();
+        // Init structures for storing information
+        List<String> nodes = new LinkedList<String>();
+        HashMap<String, LinkedList<String>> edges =
+                new HashMap<String, LinkedList<String>>();
 
-		// Open file
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(filename));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+        // Open file
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
-		// Read content
-		String line;
-		try {
+        // Read content
+        String line;
+        try {
 
-			// Read first line
-			br.readLine();
+            // Read first line
+            br.readLine();
 
-			// Read nodes (until #)
-			while (!(line = br.readLine()).startsWith("#"))
-				nodes.add(line);
+            // Read nodes (until #)
+            while (!(line = br.readLine()).startsWith("#"))
+                nodes.add(line);
 
-			// Read edges (until EOF)
-			while ((line = br.readLine()) != null) {
+            // Read edges (until EOF)
+            while ((line = br.readLine()) != null) {
 
-				String[] tmp = line.split(";");
-				LinkedList<String> list;
-				if ((list = edges.get(tmp[0])) == null) {
-					list = new LinkedList<String>();
-					edges.put(tmp[0], list);
-				}
-				list.add(tmp[1]);
-			}
-			br.close();
+                String[] tmp = line.split(";");
+                LinkedList<String> list;
+                if ((list = edges.get(tmp[0])) == null) {
+                    list = new LinkedList<String>();
+                    edges.put(tmp[0], list);
+                }
+                list.add(tmp[1]);
+            }
+            br.close();
 
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
-		// Init members
-		am  = new ArrayListMatrix(nodes.size());
-		map = new LinkedHashMap<String,Integer>();
+        // Init members
+        am = new ArrayListMatrix(nodes.size());
+        map = new LinkedHashMap<String, Integer>();
 
-		// Build nodes
-		int i=0;
-		for (String node : nodes)
-			map.put(node, i++);
+        // Build nodes
+        int i = 0;
+        for (String node : nodes)
+            map.put(node, i++);
 
-		// Build edges
-		for (String key : edges.keySet())
-			for (String value : edges.get(key))
-				am.set(map.get(key), map.get(value), 1);
-	}
+        // Build edges
+        for (String key : edges.keySet())
+            for (String value : edges.get(key))
+                am.set(map.get(key), map.get(value), 1);
+    }
 
-	/**
-	 * Returns the adjacency matrix.
-	 * @return the adjacency matrix.
-	 */
-	public AdjacencyMatrix getAdjacencyMatrix() {
-		return am;
-	}
+    /**
+     * Returns the adjacency matrix.
+     *
+     * @return the adjacency matrix.
+     */
+    public AdjacencyMatrix getAdjacencyMatrix() {
+        return am;
+    }
 
-	/**
-	 * Returns the node name mapping. This map associates a node name to its matrix 
-	 * index.
-	 * @return a map associating a node name to its index in the matrix.
-	 */
-	public HashMap<String,Integer> getNodeMapping() {
-		return map;
-	}
+    /**
+     * Returns the node name mapping. This map associates a node name to its matrix
+     * index.
+     *
+     * @return a map associating a node name to its index in the matrix.
+     */
+    public HashMap<String, Integer> getNodeMapping() {
+        return map;
+    }
 }
--- src/ch/heigvd/wem/linkanalysis/LinkAnalysis.java
+++ src/ch/heigvd/wem/linkanalysis/LinkAnalysis.java
@@ -7,49 +7,52 @@ import java.util.Vector;
  */
 public class LinkAnalysis {
 
-	/**
-	 * Calculates and returns the hub vector.
-	 * @param m Adjacency matrix.
-	 * @param ac Auhtority vector of the previous step.
-	 * @return Hub vector.
-	 */
-	public static Vector<Double> calculateHc (AdjacencyMatrix m, Vector<Double> ac) {
+    /**
+     * Calculates and returns the hub vector.
+     *
+     * @param m  Adjacency matrix.
+     * @param ac Auhtority vector of the previous step.
+     * @return Hub vector.
+     */
+    public static Vector<Double> calculateHc(AdjacencyMatrix m, Vector<Double> ac) {
 
-		Vector<Double> result = new Vector<Double>(m.size());
-
-		/* A IMPLEMENTER */
-
-		return result; 
-	}
-
-	/**
-	 * Calculates and returns the authority vector.
-	 * @param m Adjacency matrix.
-	 * @param hc Hub of the previous step.
-	 * @return Authority vector.
-	 */
-	public static Vector<Double> calculateAc (AdjacencyMatrix m, Vector<Double> hc) {
-
-		Vector<Double> result = new Vector<Double>(m.size());
+        Vector<Double> result = new Vector<Double>(m.size());
 
 		/* A IMPLEMENTER */
 
-		return result; 
-	}
+        return result;
+    }
 
-	/**
-	 * Calculates and returns the pagerank vector.
-	 * @param m Adjacency matrix.
-	 * @param pr Pagerank vector of the previous step.
-	 * @return Pagerank vector.
-	 */
-	public static Vector<Double> calculatePRc (AdjacencyMatrix m, Vector<Double> pr) {
+    /**
+     * Calculates and returns the authority vector.
+     *
+     * @param m  Adjacency matrix.
+     * @param hc Hub of the previous step.
+     * @return Authority vector.
+     */
+    public static Vector<Double> calculateAc(AdjacencyMatrix m, Vector<Double> hc) {
 
-		Vector<Double> result = new Vector<Double>(m.size());
+        Vector<Double> result = new Vector<Double>(m.size());
 
 		/* A IMPLEMENTER */
 
-		return result; 
-	}
-	
+        return result;
+    }
+
+    /**
+     * Calculates and returns the pagerank vector.
+     *
+     * @param m  Adjacency matrix.
+     * @param pr Pagerank vector of the previous step.
+     * @return Pagerank vector.
+     */
+    public static Vector<Double> calculatePRc(AdjacencyMatrix m, Vector<Double> pr) {
+
+        Vector<Double> result = new Vector<Double>(m.size());
+
+		/* A IMPLEMENTER */
+
+        return result;
+    }
+
 }
